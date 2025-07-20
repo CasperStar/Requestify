a7b16331a47ccc25339430643b7f84f4f65ee704
casperstar <casper.star@live.nl>
2025-07-20 14:36:37 +0200

    Added starting page

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Requestify</h1>
+      <p>Welcome to Requestify! Search and request your favorite Spotify tracks.</p>
+    </div>
+  );
+}
+
+export default App;
